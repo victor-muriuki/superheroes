5315be31359633434b189c00ca0668ddca17235c
+ <+>
2024-01-28 19:01:31 +0300

    creating routes

--- python-code-challenge-superheroes/python-code-challenge-superheroes/code-challenge/app/app.py
+++ python-code-challenge-superheroes/python-code-challenge-superheroes/code-challenge/app/app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
+
 
 from models import db, Hero, HeroPower, Power
 
@@ -17,6 +18,17 @@ db.init_app(app)
 def home():
     return '<h1>This is landing pasge</h1>'
 
+@app.route('/add_hero', methods = ['POST'])
+def add_hero():
+    data = request.json
 
-if __name__ == '__main__':
-    app.run(port=5555)
+    new_hero = Hero(name = data['name'])
+    db.session.add(new_hero)
+    db.session.commit()
+
+
+    return jsonify({"message": "Hero added successfully"}),
+
+
+    if __name__ == '__main__':
+        app.run(port=5555)
